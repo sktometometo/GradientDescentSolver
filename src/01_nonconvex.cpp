2846e40ccc7679da91c3bc348ed8b6f510684668
Koki Shinjo <sktometometo@gmail.com>
2020-05-22 19:10:00 +0900

    add nonconvex sample

--- src/01_nonconvex.cpp
+++ src/01_nonconvex.cpp
@@ -9,20 +9,20 @@
 //
 #include "gradient_descent_solver.h"
 
-double fn( Eigen::VectorXd x, Eigen::MatrixXd A, double b, Eigen::VectorXd c )
+double fn( Eigen::VectorXd x, Eigen::MatrixXd A, Eigen::VectorXd b, double c )
 {
-    double t = ( b - x.transpose * A * x - c.transpose * x );
+    double t = ( x.transpose() * A * x + b.dot( x ) + c );
     return t * t;
 }
 
-Eigen::VectorXd dfn( Eigen::VectorXd x, Eigen::MatrixXd A, Eigen::VectorXd b )
+Eigen::VectorXd dfn( Eigen::VectorXd x, Eigen::MatrixXd A, Eigen::VectorXd b, double c )
 {
-    return - 2 * A.transpose() * b + 2 * A.transpose() * A * x;
+    return 2 * ( x.transpose() * A * x + b.dot( x ) + c ) * ( 2 * A.transpose() * x + b );
 }
 
-double errorn( Eigen::VectorXd x, Eigen::VectorXd x_star, Eigen::MatrixXd A, Eigen::VectorXd b )
+double errorn( Eigen::VectorXd x, Eigen::MatrixXd A, Eigen::VectorXd b, double c  )
 {
-    return std::abs( fn( x, A, b ) - fn( x_star, A, b ) );
+    return std::abs( dfn( x, A, b, c ).norm() );
 }
 
 int main( int argc, char **argv )
@@ -59,19 +59,19 @@ int main( int argc, char **argv )
         }
     }
 
+    std::srand(std::time(0));
 
     Eigen::MatrixXd A = Eigen::MatrixXd::Random(degree,degree) * 100;
-    Eigen::VectorXd omega_hat = Eigen::VectorXd::Ones(degree);
-    Eigen::VectorXd eps = Eigen::VectorXd::Random(degree) * 10;
-    Eigen::VectorXd b = A * omega_hat + eps;
+    Eigen::VectorXd b = Eigen::VectorXd::Random(degree) * 100;
+    double c = std::rand() * 100.0 / RAND_MAX;
     Eigen::VectorXd x_start = Eigen::VectorXd::Random(degree) * 100;
 
     std::cout << std::fixed;
 
     GradientDescentSolver<Eigen::VectorXd> solver( debug );
-    solver.setf( boost::bind( fn, _1, A, b ) );
-    solver.setdf( boost::bind( dfn, _1, A, b ) );
-    solver.seterror( boost::bind( errorn, _1, omega_hat, A, b ) );
+    solver.setf( boost::bind( fn, _1, A, b, c ) );
+    solver.setdf( boost::bind( dfn, _1, A, b, c ) );
+    solver.seterror( boost::bind( errorn, _1, A, b, c ) );
     std::vector<Eigen::VectorXd> trajectory;
     Eigen::VectorXd x_target = solver.solve( x_start, trajectory, error );
 
